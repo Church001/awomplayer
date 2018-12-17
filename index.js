7935bb7194dc99909df5df8f22638954abbdec28
church <simonreuben23@gmail.com>
2018-12-17 08:28:41 +0100

    creating more compoonents and containers

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import AppNavigator from './App';
 import {name as appName} from './app.json';
 import Youtube from './src/components/Youtube'
 
-AppRegistry.registerComponent(appName, () => Youtube);
+AppRegistry.registerComponent(appName, () => AppNavigator);
 
---
+++ src/screens/HomeStack.js
@@ -0,0 +1,9 @@
+import {createDrawerNavigator} from "react-navigation";
+
+const HomeStack = createDrawerNavigator(
+    {
+
+    },
+);
+
+export default HomeStack;
